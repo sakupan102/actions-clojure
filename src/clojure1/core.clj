e7cd717564e77acc57a131e39162447b366beffb
Ryutaro Okada <rokada@xcoo.jp>
2023-09-28 11:14:57 +0900

    マージされたことを確認する

--- src/clojure1/core.clj
+++ src/clojure1/core.clj
@@ -1,10 +1,14 @@
 (ns clojure1.core)
 (require '[environ.core :refer [env]])
+(require '[clj-http.client :as client])
 
 (defn -main
   "I don't do a whole lot."
   []
   (println "Hello, World!"))
 
-(println (env :env-1))
+(println (env :number))
+(println (env :sender))
+(println (client/get "http://example.com"))
+
 
